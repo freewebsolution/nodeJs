04545457d63d74ef05b503d1c011999e41cdb169
lucio Ticali <info@freewebsolution.it>
2021-03-31 19:15:31 +0200

    Crea un semplice server web

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,27 @@
-console.log('Hello World!')
+const http = require('http')
+const userData = [
+    {
+        "id": 1,
+        "name": "Lucio",
+        "username": "ticalilucio@gmail.it"
+    },
+    {
+        "id": 2,
+        "name": "Mattia",
+        "username": "Mattia@libero.com"
+    },
+    {
+        "id": 3,
+        "name": "Giorgia",
+        "username": "Giorgia@virgilio.com"
+    }
+]
+
+const app = http.createServer((request, response) => {
+    response.writeHead(200, { 'Content-type': 'text/plain' })
+    response.end(JSON.stringify(userData))
+})
+
+const PORT = 3001
+app.listen(PORT)
+console.log(`Server running on port ${PORT}`)
