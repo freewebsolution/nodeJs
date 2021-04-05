e858cf143a8123a97f97e181d95f3892163f8667
lucio Ticali <info@freewebsolution.it>
2021-04-05 14:46:57 +0200

    Backend connesso al database MongoDb

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,25 @@ app.use(express.json())
 const cors = require('cors')
 app.use(express.static('build'))
 app.use(cors())
+const mongoose = require('mongoose')
+const password = 'secret123456'
+const url = `mongodb+srv://lucioTicali:${password}@cluster0.ncufe.mongodb.net/note-db?retryWrites=true&w=majority`
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+const noteSchema = new mongoose.Schema({
+    tema: String,
+    date: Date,
+    important: Boolean,
+    giorno: Date,
+    ora: String
+})
+noteSchema.set('toJSON',{
+    transform:(document, returnedObject)=> {
+        returnedObject.id = returnedObject._id.toString()
+        delete returnedObject.__v
+        delete returnedObject._id
+    }
+})
+const Note = mongoose.model('Note', noteSchema)
 //Middleware
 const requestLogger = (request, response, next) => {
     console.log('Method:', request.method)
@@ -14,57 +33,16 @@ const requestLogger = (request, response, next) => {
 }
 app.use(requestLogger)
 
-
-let notes = [
-    {
-        "tema": "partita juve",
-        "giorno": "2021-04-06T22:00:00.000Z",
-        "ora": "18:45",
-        "data": "2021-03-23T17:41:56.622Z",
-        "important": true,
-        "id": 1
-    },
-    {
-        "id": 2,
-        "tema": "chiamare andrea",
-        "giorno": "2021-03-25T23:00:00.000Z",
-        "ora": "19:45",
-        "data": "2021-03-23T17:42:19.802Z",
-        "important": false
-    },
-    {
-        "tema": "partita juve",
-        "giorno": "2021-03-26T23:00:00.000Z",
-        "ora": "18:45",
-        "data": "2021-03-23T17:56:32.768Z",
-        "important": false,
-        "id": 3
-    },
-    {
-        "tema": "revisione auto",
-        "giorno": "2021-03-27T23:00:00.000Z",
-        "ora": "09:30",
-        "data": "2021-03-23T17:57:46.886Z",
-        "important": true,
-        "id": 4
-    },
-    {
-        "tema": "Riunione a scuola",
-        "giorno": "2021-03-29T22:00:00.000Z",
-        "ora": "15:30",
-        "data": "2021-03-23T18:00:05.238Z",
-        "important": true,
-        "id": 5
-    }
-]
-
 //Home page
 app.get('/', (request, response) => {
     response.send('<h1>Hello world!</h1>')
 })
 //Recupera tutte le note
 app.get('/api/notes', (request, response) => {
-    response.json(notes)
+    Note.find({}).then(notes=>{
+          response.json(notes)  
+    })
+
 })
 
 //Recupera singola nota
