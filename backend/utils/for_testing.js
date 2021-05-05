47ecf964a08a96f1e5a9cb2e878718441bd400be
lucio Ticali <info@freewebsolution.it>
2021-05-05 12:21:03 +0200

    Supertest

--- backend/utils/for_testing.js
+++ backend/utils/for_testing.js
@@ -5,14 +5,16 @@ const palindrome = (string) => {
         .join('')
 }
 
-const average = (Array) => {
+const average = (array) => {
     const reducer = (sum, item) => {
         return sum + item
     }
-    return array.reduce(reducer, 0) / array.length
+    return array.length === 0
+        ? 0
+        : array.reduce(reducer, 0) / array.length
 }
 
 module.exports = {
     palindrome,
-    average,
+    average
 }
