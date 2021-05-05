39dd0cebb27df32d3c2fd20c967aa20b2c288209
lucio Ticali <info@freewebsolution.it>
2021-05-05 16:04:42 +0200

    Inizializzazione del database prima di fare il test

--- backend/test/note_api.test.js
+++ backend/test/note_api.test.js
@@ -1,9 +1,32 @@
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
-
 const api = supertest(app)
 
+const Note = require('../models/note')
+const initialNotes = [
+    {
+        tema: 'HTML is easy',
+        date: new Date(),
+        important: false,
+        giorno: '2021-03-29T22:00:00.000+00:00',
+        ora: '15:00',
+    },
+    {
+        tema: 'Appuntamento da ore 10:00 da Riccardo Compagnoni',
+        date: new Date(),
+        important: true,
+        giorno: '2021-05-29T22:00:00.000+00:00',
+        ora: '10:00',
+    },
+]
+beforeEach(async () => {
+    await Note.deleteMany({})
+    let noteObject = new Note(initialNotes[0])
+    await noteObject.save()
+    noteObject = new Note(initialNotes[1])
+    await noteObject.save()
+})
 test('notes are returned as json', async () => {
     await api
         .get('/api/notes')
@@ -13,13 +36,16 @@ test('notes are returned as json', async () => {
 test('there are two notes', async () => {
     const response = await api.get('/api/notes')
 
-    expect(response.body).toHaveLength(2)
+    expect(response.body).toHaveLength(initialNotes.length)
 })
 
 test('the first note is about HTTP methods', async () => {
     const response = await api.get('/api/notes')
 
-    expect(response.body[0].tema).toBe('HTML is easy')
+    const contents = response.body.map(r =>r.tema)
+    expect(contents).toContain(
+        'Appuntamento da ore 10:00 da Riccardo Compagnoni'
+    )
 })
 
 afterAll(() => {
