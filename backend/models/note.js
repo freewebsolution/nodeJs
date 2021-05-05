73e280f3d38cb0ad976bb4edc750f74cc62d7ca3
lucio Ticali <info@freewebsolution.it>
2021-05-05 10:22:39 +0200

    Correto file models/note.js

--- backend/models/note.js
+++ backend/models/note.js
@@ -1,11 +1,19 @@
 const mongoose = require('mongoose')
 
 const noteSchema = new mongoose.Schema({
-    tema: String,
+    tema: {
+        type: String,
+        required: true,
+        minlength: 5
+    },
     date: Date,
     important: Boolean,
     giorno: Date,
-    ora: String
+    ora: String,
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    }
 })
 
 noteSchema.set('toJSON', {
